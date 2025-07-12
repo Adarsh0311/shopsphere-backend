e4b87fad9d804631cb7676a07ec9703597ef4c51
adarsh0311 <adarshghai6@gmail.com>
2025-07-12 15:50:59 -0500

    feat: Add ProductRepository for CRUD operations and custom queries

---
+++ src/main/java/com/shopsphere/repository/ProductRepository.java
@@ -0,0 +1,26 @@
+package com.shopsphere.repository;
+
+import com.shopsphere.model.Product;
+import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, String> {
+    Optional<Product> findByNameIgnoreCase(String name);
+
+    //List<Product> findByCategoryId(String categoryId);
+
+    List<Product> findByPriceBetween(BigDecimal minPrice, BigDecimal maxPrice);
+
+    List<Product> findByStockQuantityLessThanEqual(Integer stockQuantity);
+
+    @Query("SELECT p FROM Product p WHERE p.name LIKE %:searchTerm%")
+    List<Product> searchProducts(@Param("searchTerm") String searchTerm);
+
+}
